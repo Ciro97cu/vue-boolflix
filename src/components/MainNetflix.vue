989f3e72a8e5006f1f590be85342c5562bc6bc3c
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-21 08:55:27 +0200

    layout changes

--- src/components/MainNetflix.vue
+++ src/components/MainNetflix.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-3 p-3" v-for="item in arrayFilm" :key="item.id">
-        <p>Titolo: {{ item.title }}</p>
-        <p>Titolo originale: {{ item.original_title }}</p>
-        <img
-          :src="displayFlags(item.original_language)"
-          :alt="item.original_language"
-        />
-        <p>Voto: {{ item.vote_average }}</p>
-      </div>
-      <div class="col-sm-3 p-3" v-for="item in arraySerie" :key="item.id">
-        <p>Titolo: {{ item.name }}</p>
-        <p>Titolo originale: {{ item.original_name }}</p>
-        <img
-          :src="displayFlags(item.original_language)"
-          :alt="item.original_language"
-        />
-        <p>Voto: {{ item.vote_average }}</p>
+  <main>
+    <div class="container-sm p-5">
+      <div class="row justify-content-center gap-3">
+        <div class="col-sm-3 p-3" v-for="item in arrayFilm" :key="item.id">
+          <p>Titolo: {{ item.title }}</p>
+          <p>Titolo originale: {{ item.original_title }}</p>
+          <img
+            :src="displayFlags(item.original_language)"
+            :alt="item.original_language"
+          />
+          <p>Voto: {{ item.vote_average }}</p>
+        </div>
+        <div class="col-sm-3 p-3" v-for="item in arraySerie" :key="item.id">
+          <p>Titolo: {{ item.name }}</p>
+          <p>Titolo originale: {{ item.original_name }}</p>
+          <img
+            :src="displayFlags(item.original_language)"
+            :alt="item.original_language"
+          />
+          <p>Voto: {{ item.vote_average }}</p>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -36,6 +38,12 @@ export default {
         return `https://www.kidlink.org/icons/f0-gb.gif`;
       } else if (region === "ja") {
         return `https://www.kidlink.org/icons/f0-jp.gif`;
+      } else if (region === "da") {
+        return `https://www.kidlink.org/icons/f0-dk.gif`;
+      } else if (region === "zh") {
+        return `https://www.kidlink.org/icons/f0-cn.gif`;
+      } else if (region === "ko") {
+        return `https://www.kidlink.org/icons/f0-kr.gif`;
       }
       return `https://www.kidlink.org/icons/f0-${region}.gif`;
     },
@@ -44,4 +52,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main {
+  background-image: url(../assets/img/netflix-bg.png);
+  background-size: cover;
+  min-height: calc(100vh - 66px);
+
+  .col-sm-3 {
+    width: 30%;
+    background-color: rgba($color: #ccc, $alpha: 0.7);
+    color: white;
+    border: 1px solid white;
+  }
+}
 </style>
